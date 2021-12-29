348a4326cd7761591b295797b94ec802cd4db4c9
Damien Retzinger <damienwebdev@gmail.com>
2021-12-29 13:52:40 -0500

    feat: add example of basic @daffodil/design setup

--- ng11-daffodil-design/src/app/app.module.ts
+++ ng11-daffodil-design/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DaffHeroModule } from '@daffodil/design';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, DaffHeroModule],
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
   bootstrap: [AppComponent],
 })
